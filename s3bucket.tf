ca8d27b9ae5ad7ac8ec876d483def86378e83bd1
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-06-03 20:11:17 -0400

    Update s3bucket.tf

--- s3bucket.tf
+++ s3bucket.tf
@@ -1,11 +1,5 @@
 resource "aws_s3_bucket" "bucket1" {
     count=2
     tags = {
-        name="test-bucket-${count.index}"
-        }
-acl    = "private"
-versioning {
-    enabled = true
-  }
+          }
 }
-
